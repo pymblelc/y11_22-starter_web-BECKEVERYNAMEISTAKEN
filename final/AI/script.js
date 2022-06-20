945a2f0e1bbe3f627427deafdccad71f30904da2
BECKEVERYNAMEISTAKEN <ratkinson2023@pymblelc.nsw.edu.au>
2022-06-20 14:42:41 +1000

    Updated Pages

---
+++ final/AI/script.js
@@ -0,0 +1,32 @@
+let myImage = document.getElementById("myPhoto");
+let myButton = document.getElementById("btnAnalyse");
+let results = document.getElementById("myText");
+
+let imageURL = myImage.src;
+
+myButton.addEventListener("click", function () {
+    //myText.I
+    ImageAPI.analyseFaces(imageURL, function (data) {
+      //  console.log(data[0].faceAttributes.age);  
+      for (let i = 0; i < data.length; i++) {
+        let age = data[i].faceAttributes.age;
+        let lipstick = data[i].faceAttributes.makeup.lipMakeup;
+        let results = '<p>Face ' + (i + 1) + ': age is' + age + ', lipstick = '
+                        + lipstick + '</p>';
+        myText.innerHTML += results; 
+      }
+
+    });
+});
+
+
+//        finalInfo += "and has" + data[0].faceAttributes.facialhair.moustache + "moustage";
+/*
+results.innerHTML = imageURL;
+ImageAPI.annalyseFaces(imageURL, function(data){
+    for 
+}
+        let finalInfo = "Dr Hadwen looks about" + data[0].faceAttributes.age + "years old";
+
+        results.innerHTML = finalInfo;
+*/
