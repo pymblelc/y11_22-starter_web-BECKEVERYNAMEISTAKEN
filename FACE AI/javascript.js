333f3dafeda9ec5783d2af048348cb1b4363c8f1
BECKEVERYNAMEISTAKEN <ratkinson2023@pymblelc.nsw.edu.au>
2022-06-22 09:30:17 +1000

    FACEID

---
+++ FACE AI/javascript.js
@@ -0,0 +1,23 @@
+var faces; //undefined
+
+// faces = [
+    // "hello",
+    // "world",
+// ];
+
+faces = [
+{
+    "mo" : 1,
+    "age" : 43,
+    "style" : 0,
+},
+{
+    "mo" : 0,
+    "age" : 16,
+    "style" : 32,
+},
+];
+
+// elements for each list item
+faces[0].age //43
+faces[1].style //32
