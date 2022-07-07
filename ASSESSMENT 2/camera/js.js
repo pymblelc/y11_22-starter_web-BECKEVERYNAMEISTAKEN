de337018f34d1e9dcd0762bc48d229b1f6fdc135
BECKEVERYNAMEISTAKEN <ratkinson2023@pymblelc.nsw.edu.au>
2022-07-07 16:27:32 +1000

    more

--- ASSESSMENT 2/camera/js.js
+++ ASSESSMENT 2/camera/js.js
@@ -1,38 +1,49 @@
-var constraints = {video: {facingMode:"user "}, audio: false};
 
+// Set constraints for the video stream
+var constraints = { video: { facingMode: "user" }, audio: false };
+
+// Define constants
 const cameraView = document.querySelector("#camera--view"),
-    cameraOutput = document.querySelector("camera--output"),
+    cameraOutput = document.querySelector("#camera--output"),
     cameraSensor = document.querySelector("#camera--sensor"),
     cameraTrigger = document.querySelector("#camera--trigger"),
-    cameraClear = document.querySelector("#camera--clear"),
-    cameraAnalyse = document.querySelector("#camera--analyse")
+    cameraAnalyse = document.querySelector("#camera--analyse"),
+    cameraClear = document.querySelector("#camera--clear");
 
-    function cameraStart() {
-        navigator.mediaDevices
-            .getUserMedia(constraints)
-            .then(function(stream) {
-            track = stream.getTracks()[0];
-            cameraView.srcObject = stream;
-        })
-        .catch(function(error) {
-            console.error("Oops. Something is broken.", error);
-        });
-    }
+// Access the device camera and stream to cameraView
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+        track = stream.getTracks()[0];
+        cameraView.srcObject = stream;
+    })
+    .catch(function(error) {
+        console.error("Oops. Something is broken.", error);
+    });
+}
+// Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+};
 
-    cameraTrigger.onclick = function() {
-        cameraSensor.width = cameraView.videoWidth;
-        cameraSensor.height = cameraView.videoHeight;
-        cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-        cameraOutput.src = cameraSensor.toDataURL("image/webp");
-        cameraOutput.classList.add("taken");
-    };
+cameraAnalyse.onclick = function() {
+    document.querySelector("taken");
+    
+};
 
-    window.addEventListener("load", cameraStart, false);
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
 
+
+    //clear the camera
 cameraClear.onclick = function() {
     cameraSensor.width = "#fff"
     cameraSensor.height = "#fff";
 }
 let results =document.getElementById("myText");
 
-cameraAnalyse.onclick = ImageAPI.analyseImage() ;
